690630b016d56e62c1badf260c788009bf030b63
danieldraganweb <110283149+danieldraganweb@users.noreply.github.com>
2023-06-14 21:08:57 +0200

    delete boilerplate css

--- app/page.tsx
+++ app/page.tsx
@@ -6,6 +6,7 @@ export default function Home() {
     <main className={styles.main}>
       <div className={styles.grid}>
         <h1 className={styles.title}>Welcome to my page!</h1>
+
         <p className={styles.description}></p>
       </div>
     </main>
