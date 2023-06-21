9417c637912a3a11e293d000c54155d7c130f04c
danieldraganweb <110283149+danieldraganweb@users.noreply.github.com>
2023-06-21 21:47:42 +0200

    add instagram svg component to footer

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,10 +1,14 @@
 import styles from "../styles/Footer.module.scss";
+import InstagramSVG from "./InstagramSVG";
 
 function Footer() {
   return (
-    <div className={styles.main}>
-      <p className={styles.copyright}>&copy; 2023 Darius Filip</p>
-    </div>
+    <>
+      <div className={styles.main}>
+        <InstagramSVG />
+        <p className={styles.copyright}>&copy; 2023 Darius Filip</p>
+      </div>
+    </>
   );
 }
 
