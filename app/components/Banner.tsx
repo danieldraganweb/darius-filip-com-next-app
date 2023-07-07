783d0b452fadf967a5f271cad0b455d320426948
danieldraganweb <110283149+danieldraganweb@users.noreply.github.com>
2023-07-07 13:49:02 +0200

    add useEffect for image loading and smooth effect

--- app/components/Banner.tsx
+++ app/components/Banner.tsx
@@ -1,22 +1,23 @@
 "use client";
 import Image from "next/image";
 import styles from "../styles/components/Banner.module.scss";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 function BannerImage() {
   const images = [
-    // "/main-img-1.jpg",
     "/main-img-2.jpg",
-    // "/main-img-3.jpg",
     "/main-img-4.jpg",
     "/main-img-5.jpg",
     "/main-img-6.jpg",
     "/main-img-7.jpg",
   ];
-  const randomIndex = Math.floor(Math.random() * images.length);
-  const randomImage = images[randomIndex];
+  const [randomImage, setRandomImage] = useState("");
   const [loading, setLoading] = useState(true);
-  loading === true ? console.log("loading") : console.log("loaded");
+
+  useEffect(() => {
+    const randomIndex = Math.floor(Math.random() * images.length);
+    setRandomImage(images[randomIndex]);
+  }, []);
 
   return (
     <div className={styles.main}>
@@ -42,7 +43,7 @@ function BannerImage() {
               ? `${styles["scale-110"]} ${styles["blur-2xl"]} ${styles["grayscale"]}`
               : `${styles["scale-100"]} ${styles["blur-0"]} ${styles["grayscale-0"]}`
           }`}
-          onLoad={() => setLoading(false)}
+          // onLoad={() => setLoading(false)}
           onLoadingComplete={() => setLoading(false)}
         />
       </div>
@@ -51,3 +52,55 @@ function BannerImage() {
 }
 
 export default BannerImage;
+
+// "use client";
+// import Image from "next/image";
+// import styles from "../styles/components/Banner.module.scss";
+// import { useState, useEffect } from "react";
+
+// function BannerImage() {
+//   const images = [
+//     "/main-img-2.jpg",
+//     "/main-img-4.jpg",
+//     "/main-img-5.jpg",
+//     "/main-img-6.jpg",
+//     "/main-img-7.jpg",
+//   ];
+//   const [randomImage, setRandomImage] = useState("");
+//   const [loading, setLoading] = useState(true);
+
+//   useEffect(() => {
+//     const randomIndex = Math.floor(Math.random() * images.length);
+//     setRandomImage(images[randomIndex]);
+//   }, []);
+
+//   return (
+//     <div className={styles.container}>
+//       <div className={styles.main}>
+//         <div className={styles.img}>
+//           {randomImage && (
+//             <Image
+//               src={randomImage}
+//               alt="macarale"
+//               quality={100}
+//               width={1250}
+//               height={700}
+//               objectFit="contain"
+//               className={`${styles.container} ${styles["duration-700"]} ${
+//                 styles["ease-in-out"]
+//               } ${styles["group-hover:opacity-75"]} ${
+//                 loading
+//                   ? `${styles["scale-110"]} ${styles["blur-2xl"]} ${styles["grayscale"]}`
+//                   : `${styles["scale-100"]} ${styles["blur-0"]} ${styles["grayscale-0"]}`
+//               }`}
+//               onLoad={() => setLoading(false)}
+//               onLoadingComplete={() => setLoading(false)}
+//             />
+//           )}
+//         </div>
+//       </div>
+//     </div>
+//   );
+// }
+
+// export default BannerImage;
