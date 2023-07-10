6e65587f78f74d4dcada99cc5b676482819de4dc
danieldraganweb <110283149+danieldraganweb@users.noreply.github.com>
2023-07-11 00:00:40 +0200

    add animation on loading

--- app/loading.tsx
+++ app/loading.tsx
@@ -5,21 +5,23 @@ import styles from "./styles/components/Loading.module.scss";
 
 export default function Loading() {
   return (
-    <div className={styles.container}>
-      <div className={styles.spinnerCenter}>
-        <div className={styles.spinnerBlade}></div>
-        <div className={styles.spinnerBlade}></div>
-        <div className={styles.spinnerBlade}></div>
-        <div className={styles.spinnerBlade}></div>
-        <div className={styles.spinnerBlade}></div>
-        <div className={styles.spinnerBlade}></div>
-        <div className={styles.spinnerBlade}></div>
-        <div className={styles.spinnerBlade}></div>
-        <div className={styles.spinnerBlade}></div>
-        <div className={styles.spinnerBlade}></div>
-        <div className={styles.spinnerBlade}></div>
-        <div className={styles.spinnerBlade}></div>
+    <main className={styles.main}>
+      <div className={styles.container}>
+        <div className={styles.spinnerCenter}>
+          <div className={styles.spinnerBlade}></div>
+          <div className={styles.spinnerBlade}></div>
+          <div className={styles.spinnerBlade}></div>
+          <div className={styles.spinnerBlade}></div>
+          <div className={styles.spinnerBlade}></div>
+          <div className={styles.spinnerBlade}></div>
+          <div className={styles.spinnerBlade}></div>
+          <div className={styles.spinnerBlade}></div>
+          <div className={styles.spinnerBlade}></div>
+          <div className={styles.spinnerBlade}></div>
+          <div className={styles.spinnerBlade}></div>
+          <div className={styles.spinnerBlade}></div>
+        </div>
       </div>
-    </div>
+    </main>
   );
 }
