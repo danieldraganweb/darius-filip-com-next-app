9f2c2712383d229dffea640c25e4439e7b1d326e
danieldraganweb <110283149+danieldraganweb@users.noreply.github.com>
2023-07-12 16:41:28 +0200

    style contact form for mobile

--- app/components/ContactForm.tsx
+++ app/components/ContactForm.tsx
@@ -36,82 +36,84 @@ function ContactForm() {
 
   return (
     <>
-      <div className={styles.container}>
-        <div className={styles.textContainer}>
-          <h1>Let&apos;s get in touch !</h1>
-          <p className={styles.description}>
-            Whether you&apos;re interested in booking a photo shoot, have a
-            question about my services, or just want to say hello, I&apos;m
-            always happy to hear from you. Fill out the contact form or send me
-            an WhatsApp message, and I&apos;ll get back to you as soon as
-            possible!
-          </p>
-          <Link
-            href="https://api.whatsapp.com/send/?phone=+4560515731&text&type=phone_number&app_absent=0"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <button className={styles.buttonWApp}>
-              Prefer to chat? @WhatsApp
-            </button>
-          </Link>
+      <main className={styles.main}>
+        <div className={styles.container}>
+          <div className={styles.textContainer}>
+            <h1>Let&apos;s get in touch !</h1>
+            <p className={styles.description}>
+              Whether you&apos;re interested in booking a photo shoot, have a
+              question about my services, or just want to say hello, I&apos;m
+              always happy to hear from you. Fill out the contact form or send
+              me an WhatsApp message, and I&apos;ll get back to you as soon as
+              possible!
+            </p>
+            <Link
+              href="https://api.whatsapp.com/send/?phone=+4560515731&text&type=phone_number&app_absent=0"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <button className={styles.buttonWApp}>
+                Prefer to chat? @WhatsApp
+              </button>
+            </Link>
+          </div>
+          <div className={styles.formContainer}>
+            <form onSubmit={handleSubmit}>
+              <div>
+                <label htmlFor="name"></label>
+                <input
+                  type="text"
+                  id="name"
+                  name="name"
+                  value={name}
+                  onChange={handleInputChange}
+                  required
+                  placeholder="Name"
+                />
+              </div>
+              <div>
+                <label htmlFor="email"></label>
+                <input
+                  type="email"
+                  id="email"
+                  name="email"
+                  value={email}
+                  onChange={handleInputChange}
+                  required
+                  placeholder="Email"
+                />
+              </div>
+              <div>
+                <label htmlFor="Subject"></label>
+                <input
+                  type="text"
+                  id="subject"
+                  name="subject"
+                  value={subject}
+                  onChange={handleInputChange}
+                  required
+                  placeholder="Subject"
+                />
+              </div>
+              <div>
+                <label htmlFor="Message"></label>
+                <br />
+                <textarea
+                  id="message"
+                  name="message"
+                  value={message}
+                  onChange={handleInputChange}
+                  required
+                  placeholder="Message"
+                />
+              </div>
+              <div className={styles.buttonContainer}>
+                <button type="submit">{status}</button>
+              </div>
+            </form>
+          </div>
         </div>
-        <div className={styles.formContainer}>
-          <form onSubmit={handleSubmit}>
-            <div>
-              <label htmlFor="name"></label>
-              <input
-                type="text"
-                id="name"
-                name="name"
-                value={name}
-                onChange={handleInputChange}
-                required
-                placeholder="Name"
-              />
-            </div>
-            <div>
-              <label htmlFor="email"></label>
-              <input
-                type="email"
-                id="email"
-                name="email"
-                value={email}
-                onChange={handleInputChange}
-                required
-                placeholder="Email"
-              />
-            </div>
-            <div>
-              <label htmlFor="Subject"></label>
-              <input
-                type="text"
-                id="subject"
-                name="subject"
-                value={subject}
-                onChange={handleInputChange}
-                required
-                placeholder="Subject"
-              />
-            </div>
-            <div>
-              <label htmlFor="Message"></label>
-              <br />
-              <textarea
-                id="message"
-                name="message"
-                value={message}
-                onChange={handleInputChange}
-                required
-                placeholder="Message"
-              />
-            </div>
-            <div className={styles.buttonContainer}>
-              <button type="submit">{status}</button>
-            </div>
-          </form>
-        </div>
-      </div>
+      </main>
     </>
   );
 }
