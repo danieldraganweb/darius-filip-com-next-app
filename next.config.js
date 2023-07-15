4be3b264bf7da96c7f70fda4e32b71648544c193
danieldraganweb <110283149+danieldraganweb@users.noreply.github.com>
2023-07-15 10:04:34 +0200

    acces token defined in next config for moudule exports

--- next.config.js
+++ next.config.js
@@ -4,6 +4,11 @@ const path = require("path");
 module.exports = {
   sassOptions: {
     includePaths: [path.join(__dirname, "styles")],
+    env: {
+      NEXT_PUBLIC_CONTENTFUL_SPACE_ID: "zh7t5pom8ccw",
+      NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN:
+        "HBkgA0h_Q4IS0DoD436LAfMpvdsO4-SM98E6Sz3hEEM",
+    },
   },
 };
 const nextConfig = {
