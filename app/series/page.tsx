f2562746aa8adb34897bc1abc0138fd5b5065e7a
danieldraganweb <110283149+danieldraganweb@users.noreply.github.com>
2023-07-15 09:27:03 +0200

    Vercel deploy test

--- app/series/page.tsx
+++ app/series/page.tsx
@@ -1,8 +1,6 @@
 "use client";
 import React from "react";
-import Image from "next/image";
 import styles from "../styles/pages/series.module.scss";
-import Gallery from "../components/Gallery";
 import Banner from "../components/Banner";
 
 function Series() {
