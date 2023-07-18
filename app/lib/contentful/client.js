eed48dc7b9d4db21542081e1e1b4ad1878955760
danieldraganweb <110283149+danieldraganweb@users.noreply.github.com>
2023-07-18 16:29:25 +0200

    add path to client.js

--- app/lib/contentful/client.js
+++ app/lib/contentful/client.js
@@ -4,3 +4,5 @@ export const client = contentful.createClient({
   space: process.env.NEXT_PUBLIC_CONTENTFUL_SPACE_ID,
   accessToken: process.env.NEXT_PUBLIC_CONTENTFUL_ACCESS_TOKEN,
 });
+
+// Path: app\pages\index.js
