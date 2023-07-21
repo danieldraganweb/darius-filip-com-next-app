6908f8eca2747afa5784017ac25df792b568f1c9
danieldraganweb <110283149+danieldraganweb@users.noreply.github.com>
2023-07-21 23:10:24 +0200

    add classes to slug page and style scss file

--- app/series/[slug]/page.tsx
+++ app/series/[slug]/page.tsx
@@ -28,41 +28,46 @@ const GalleryPage = (props: any) => {
   const { title, loading, entry } = useEntry(props.params.slug);
 
   return (
-    <div>
-      <p>Gallery Page - {title}</p>
-      <p>{loading ? "Loading..." : null}</p>
-
-      {entry.fields?.images.length > 0
-        ? entry.fields?.images?.map((item: any, i: number) => {
-            return (
-              <div key={i}>
-                <Image
-                  loading="eager"
-                  src={"https:" + item.fields.file.url}
-                  alt={`Recent Image ${i}`}
-                  width={300}
-                  height={300}
-                  layout="responsive"
-                  quality={100}
-                  priority={true}
-                  blurDataURL="/main-img-1.jpg"
-                  style={{
-                    borderRadius: "0.2em",
-                    boxShadow: "20 0 20px rgba(0, 0, 0, 0.2)",
-                  }}
-                  className={` ${styles["duration-700"]} ${
-                    styles["ease-in-out"]
-                  } ${styles["group-hover:opacity-75"]} ${
-                    loading
-                      ? `${styles["scale-110"]} ${styles["blur-2xl"]} ${styles["grayscale"]}`
-                      : `${styles["scale-100"]} ${styles["blur-0"]} ${styles["grayscale-0"]}`
-                  }`}
-                />
-              </div>
-            );
-          })
-        : null}
-    </div>
+    <main className={styles.main}>
+      <div className={styles.container}>
+        <h1 className={styles.title}>{title}</h1>
+        {/* <p>{loading ? "Loading..." : null}</p> */}
+        {entry.fields?.images.length > 0
+          ? entry.fields?.images?.map((item: any, i: number) => {
+              return (
+                <div key={i} className={styles.imgContainer}>
+                  <ul className={styles.imgList}>
+                    <li className={styles.imgItem}>
+                      <Image
+                        loading="eager"
+                        src={"https:" + item.fields.file.url}
+                        alt={`Recent Image ${i}`}
+                        width={300}
+                        height={300}
+                        layout="responsive"
+                        quality={100}
+                        priority={true}
+                        blurDataURL="/main-img-1.jpg"
+                        style={{
+                          borderRadius: "0.2em",
+                          boxShadow: "20 0 20px rgba(0, 0, 0, 0.2)",
+                        }}
+                        className={` ${styles["duration-700"]} ${
+                          styles["ease-in-out"]
+                        } ${styles["group-hover:opacity-75"]} ${
+                          loading
+                            ? `${styles["scale-110"]} ${styles["blur-2xl"]} ${styles["grayscale"]}`
+                            : `${styles["scale-100"]} ${styles["blur-0"]} ${styles["grayscale-0"]}`
+                        }`}
+                      />
+                    </li>
+                  </ul>
+                </div>
+              );
+            })
+          : null}
+      </div>
+    </main>
   );
 };
 
