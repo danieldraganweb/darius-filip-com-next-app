60e6b79aa5319b7ee58a405817dabe44d454b8f5
danieldraganweb <110283149+danieldraganweb@users.noreply.github.com>
2023-07-22 00:03:43 +0200

    styles shop pahe, add back to home button

--- app/bookshop/page.tsx
+++ app/bookshop/page.tsx
@@ -1,19 +1,16 @@
 import React from "react";
 import styles from "../styles/pages/bookshop.module.scss";
 import Image from "next/image";
+import Link from "next/link";
 
 function Bookshop() {
   return (
-    <main>
+    <main className={styles.main}>
       <div className={styles.container}>
-        <h1 className={styles.title}>Bookshop</h1>
-        <Image
-          src="/OIP.jpeg"
-          alt="logo"
-          width={300}
-          height={300}
-          quality={100}
-        />
+        <h1 className={styles.title}>Coming soon!</h1>
+        <Link href="/">
+          <button className={styles.button}>Back to Home</button>
+        </Link>
       </div>
     </main>
   );
